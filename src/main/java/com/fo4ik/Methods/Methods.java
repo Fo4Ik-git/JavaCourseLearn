62b0d05932322b2ee39512c562e956432261b8d1
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-03-03 20:46:48 +0100

    Add methods

---
+++ src/main/java/com/fo4ik/Methods/Methods.java
@@ -0,0 +1,31 @@
+package com.fo4ik.Methods;
+
+public class Methods {
+
+    //Method with no return type and no parameters
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+    public static void printHello(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public void printHello2() {
+        System.out.println("Hello");
+    }
+
+    
+
+    public static void main(String[] args) {
+        printHello();
+        printHello("John");
+        //printHello2();
+
+        Methods methods = new Methods();
+        methods.printHello2();
+    }
+
+
+
+
+}
