7ae3e7721501d9d3245c4d6e0988154809f423f4
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-08-03 12:43:10 +0200

    write ThreadLifeCycle

--- src/main/java/com/fo4ik/learn/multithreading/ThreadLifeCycle.java
+++ src/main/java/com/fo4ik/learn/multithreading/ThreadLifeCycle.java
@@ -6,6 +6,23 @@ public class ThreadLifeCycle {
      */
 
     public static void main(String[] args) {
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("MyThread name: " + Thread.currentThread().getName());
+            }
+        });
+        myThread.start();
+        myThread.setPriority(10); //Set priority to thread (1 - 10)
 
+        /*try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
+        //But if we want run myThread earlier than main thread we can use Thread.sleep() method
+
+        Thread.yield(); //This method will pause current thread and give a chance to other threads to execute
+        System.out.println("Main thread");
     }
 }
